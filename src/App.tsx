b9122eb0a41e6c4eb1eb13fa327805ceca19b893
DavidMaNYC <DavidmaBK@gmail.com>
2024-08-05 15:46:18 -0400

    added toast container to routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   return (
     <Router>
+      <ToastContainer />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
