b6c90e35817ccc9f3eddae5d64835ff7fabb8a9e
DavidMaNYC <DavidmaBK@gmail.com>
2024-08-05 16:03:44 -0400

    fixed JSON response

--- server/src/redis.ts
+++ server/src/redis.ts
@@ -4,6 +4,9 @@ const redis = new Redis({
   host: process.env.REDIS_HOST,
   port: parseInt(process.env.REDIS_PORT || "6379", 10),
   password: process.env.REDIS_PASSWORD || undefined,
+  tls: {
+    rejectUnauthorized: false, // Set to false to accept self-signed certificates (optional, depends on your security requirements)
+  },
 });
 
 redis.on("error", (err) => {
