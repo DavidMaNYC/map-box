1a8ef616886eed47230fca1695106db5f7a7879a
DavidMaNYC <DavidmaBK@gmail.com>
2024-08-05 15:05:10 -0400

    Add property to redis ts to handle for tls

--- server/src/redis.ts
+++ server/src/redis.ts
@@ -4,6 +4,13 @@ const redis = new Redis({
   host: process.env.REDIS_HOST,
   port: parseInt(process.env.REDIS_PORT || "6379", 10),
   password: process.env.REDIS_PASSWORD || undefined,
+  tls: {
+    rejectUnauthorized: false, // Set to false to accept self-signed certificates (optional, depends on your security requirements)
+  },
+});
+
+redis.on("error", (err) => {
+  console.error("Redis error:", err);
 });
 
 export default redis;
